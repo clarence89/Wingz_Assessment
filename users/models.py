65ce8f24508036f6d5776bdbc0e1810eaa67be30
Clarence Baluyot <akingprincess@gmail.com>
2025-08-13 20:37:44 +0800

    feat: add role as admin automatically when using createsuperuser command

--- users/models.py
+++ users/models.py
@@ -2,11 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext as _
 from users.choices import ROLE_CHOICES
+from users.managers import UserManager
 
 
 class User(AbstractUser):
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, null=True, blank=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
+    objects = UserManager()
 
     def __str__(self):
         if self.first_name:
--- users/permissions.py
+++ users/permissions.py
@@ -2,8 +2,4 @@ from rest_framework.permissions import BasePermission
 
 class IsAdminRole(BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.user
-            and request.user.is_authenticated
-            and getattr(request.user, 'role', None) == "admin"
-        )
+        return request.user.is_authenticated and request.user.role == "admin"
