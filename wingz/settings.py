38e745c4c905ddd0b803a97ef012ecb31b4db52c
Clarence Baluyot <akingprincess@gmail.com>
2025-08-13 21:03:12 +0800

    feat: create custom Pagination

--- wingz/settings.py
+++ wingz/settings.py
@@ -132,8 +132,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'wingz.paginations.CustomPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 SIMPLE_JWT = {
